646c0be2c41df3d54a82f0e4e2e291427f82b092
tfeydev <tfeydeveloper@gmail.com>
2025-11-03 21:04:02 -0300

    improvement and update of insertion_sort

--- algorithms/src/sorting/insertion_sort.rs
+++ algorithms/src/sorting/insertion_sort.rs
@@ -36,19 +36,15 @@
 /// * It performs very well on small or nearly sorted data.
 pub fn insertion_sort<T>(arr: &mut [T])
 where
-    T: PartialOrd + Copy,
+    T: Ord + Clone,
 {
-    if arr.len() < 2 {
-        return;
-    }
-
     for i in 1..arr.len() {
-        let key = arr[i];
+        let key = arr[i].clone();
         let mut j = i;
 
         // Move elements greater than key one position ahead.
         while j > 0 && arr[j - 1] > key {
-            arr[j] = arr[j - 1];
+            arr[j] = arr[j - 1].clone();
             j -= 1;
         }
 
